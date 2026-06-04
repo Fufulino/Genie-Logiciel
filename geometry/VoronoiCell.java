5587f057cfd799fdbd5e6fbed62c64a10bdac9dc
Nicos772 <jeanlouis.nicolas77@gmail.com>
2026-06-04 18:29:35 +0200

    Creation class VoronoiCell and RandomGenerator

--- geometry/VoronoiCell.java
+++ geometry/VoronoiCell.java
@@ -1,6 +1,8 @@
 package com.cergy.charitymap.geometry;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The coverage zone of one distribution center: its Voronoi cell.
@@ -14,5 +16,22 @@ import java.io.Serializable;
  * @author CharityMap team
  */
 public class VoronoiCell implements Serializable {
+    /** Serialization version identifier. */
+    private static final long serialVersionUID = 1L;
 
+    /** The center this cell is built around. */
+    private final DistributionCenter center;
+
+    /** The ordered corners of the cell polygon (Voronoi vertices). */
+    private final List<Point> corners;
+
+    /**
+     * Builds a coverage zone for a given center.
+     *
+     * @param center the center at the heart of this cell
+     */
+    public VoronoiCell(DistributionCenter center) {
+        this.center = center;
+        this.corners = new ArrayList<>();
+    }
 }
--- service/RandomGenerator.java
+++ service/RandomGenerator.java
@@ -1,4 +1,28 @@
 package com.cergy.charitymap.service;
+
+import java.util.Random;
+
+import com.cergy.charitymap.model.Point;
+
 public class RandomGenerator {
-    
+    /** Source of randomness. */
+    private final Random random;
+
+    /** The width of the generation area. */
+    private final double width;
+
+    /** The height of the generation area. */
+    private final double height;
+
+    /**
+     * Builds a generator for a given area size.
+     *
+     * @param width  the width of the area (for example 1000)
+     * @param height the height of the area (for example 1000)
+     */
+    public RandomGenerator(double width, double height) {
+        this.random = new Random();
+        this.width = width;
+        this.height = height;
+    }
 }
